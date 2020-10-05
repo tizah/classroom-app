37391802b68696889265eb13a65a23e3f19750e7
tizah <davidzagi93@gmail.com>
2020-10-06 00:56:36 +0100

    added cloud storage

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3001,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoCloud:
+    "mongodb+srv://classApp:a4JtRK5jUgCCVrME@cluster0.vdqws.mongodb.net/classApp?retryWrites=true&w=majority",
   mongoUri:
     process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
@@ -13,3 +15,6 @@ const config = {
 };
 
 export default config;
+
+//databaseName = classApp
+//password = a4JtRK5jUgCCVrME
